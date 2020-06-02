3cdb2bb37773b03d0176dbd6f116fe6b360483fd
Quantum1Shadow <ua667766706@gmail.com>
2020-06-02 11:04:35 +0300

    added product model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:
+    '<div><h1>{{pageTitle}}</h1><app-products></app-products></div>'
 })
+
 export class AppComponent {
-  title = 'aterminatorshop';
+  pageTitle: string = 'Terminator shop';
 }
